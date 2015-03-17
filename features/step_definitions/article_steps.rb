1c99ac067e79777986c9cb238c0be2c408b0b58c
Elizabeth Hagearty <elizabeth@glassymedia.com>
2015-03-16 18:34:14 -0700

    cucumber passing

--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -3,6 +3,13 @@ require 'cgi'
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
 
-Given /^the following articles exist$/ do |table|
-	Article.create(table.hashes)
+Given /the following articles exist/ do |pups_table|
+  	pups_table.hashes.each do |rating|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+	    Article.create!(
+	      	id: rating['id'],
+	      	title: rating['title'],
+	    	body: rating['body'])
+  	end
 end
